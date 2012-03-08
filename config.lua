c84929bd88cb0a78b7f4e70658349cfa76a5cb73
wendell <wendell@gmail.com>
2012-03-08 09:48:18 +0800

    disable a featrue

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ local CFG = {}
 CFG.POINT = "TOPLEFT"
 CFG.OBJECTS_NUM = 5
 
-CFG.STATUS_CHANGE = false  
+CFG.STATUS_CHANGE = false --this key maybe has bug 
 CFG.WHISPER = true
 CFG.BN_WHISPER = true
 CFG.NEW_MAIL = false -- this key maybe has bug 
